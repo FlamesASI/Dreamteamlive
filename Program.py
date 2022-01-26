d9053f5c58cb48a4ed68c422e49c831510bac7f7
Ell Citron <98425920+FlamesASI@users.noreply.github.com>
2022-01-25 20:48:52 -0800

    Request to upload 1.25.22

---
+++ Program.py
@@ -0,0 +1,125 @@
+import os as _os
+import sys
+import google
+
+
+def OnlineSupport(): {
+    ## edit the rom multiplayer citra and give a command to add multiplayer to dream team
+
+}
+
+romsupport= input("Select the rom cia you want to add and inject craftyboss's code to.")
+
+ 
+## make a client to add players to the server to the mario 
+## and luigi dream team client
+## using hex editing
+## add a cli gui to edit the roms and add multiplayer
+## and add a character select to the title
+## use hex editing with nintendos github to add multiplayer
+def MultiplayerPatcher(): {
+    print("Select your rom to add multiplayer via ASM editing and add a chatroom")
+}
+MultiplayerPatcher = ASMEditing()
+MultiplayerPatcher = OnlineSupport()
+MultiplayerPatcher = MultiplayerInstaller()
+## copy the multiplayer code to the rom and add the multiplayer code to the rom
+## with hex editing
+ChatwindowDreamteam=("80E30000")
+## character controller OnlineSupport
+controlcharacterwithmultiplayer=("80E30000")
+## battle system pvp
+## add a battle system to the game
+pvpsystemwithmarioand4players=("80E30000")
+## add a chat system that looks like runescape with dream team gui
+chatscapewithmultiplayer=("80E30000")
+print("Adding support in...")
+print("Syncing with remote server...")
+print("Downloading files...")
+remoteserver=("https://nintendo3ds.com/multiplayer/")
+## where would you like to inject the hex into a cia file too and locate it
+## and add the multiplayer code to the rom
+## add a client to add players to the server to the mario
+## and luigi dream team client
+## using hex editing
+## generate a ai that keeps track of all the players
+nintendoai=("0x80E30000")
+print(nintendoai)
+print(chatscapewithmultiplayer)
+## Locate dream team cia
+dreamteamcia=("0x80E30000")
+print("Adding multiplayer to dream team, please wait...",dreamteamcia)
+print('Connecting nintendo ai to the dream team client')
+serverai=('0x80E30000')
+print("Cloning ai,",serverai)
+print("Connecting ai to the server")
+print("Done")
+print("Would you like to use the citra servers to start a game?")
+print("Yes or No")
+citra=input("")
+## write a console that connnects your 3ds to the citra servers and plays multiplayer using the dream team and nsmb2 multiplayer engine
+# and use hex editing to add online players to the game
+dreamteamcia=("0x80E30000")
+print("Adding multiplayer to dream team, please wait...",dreamteamcia)
+print('Connecting nintendo ai to the dream team client')
+serverai=('0x80E30000')
+print("Cloning ai,",serverai)
+print("Connecting ai to the server")
+print("Done")
+print("Would you like to use the citra servers to start a game?")
+print("Yes or No")
+citra=input("")
+# connect the ai to the user client and manage all connections
+## and let the ai use general deepfakes to add content to the rom
+nintendoai=("0x80E30000")
+print(nintendoai)
+print(chatscapewithmultiplayer)
+nintendoai=("0x80E30000")
+print(nintendoai)
+print(chatscapewithmultiplayer)
+# connect the ai to the user client and manage all connections
+## and let the ai use general deepfakes to add content to the rom
+## and add a character select to the title
+## use hex editing with nintendos github to add multiplayer
+## add a client to add players to the server to the mario
+## and luigi dream team client
+## using hex editing
+## generate a ai that keeps track of all the players
+nintendoai=("0x80E30000")
+print(nintendoai)
+print(chatscapewithmultiplayer)
+## Locate dream team cia
+dreamteamcia=("0x80E30000")
+print("Adding multiplayer to dream team, please wait...",dreamteamcia)
+print('Connecting nintendo ai to the dream team client')
+serverai=('0x80E30000')
+print("Cloning ai,",serverai)
+print("Connecting ai to the server")
+print("Done")
+print("Would you like to use the citra servers to start a game?")
+print("Yes or No")
+
+## write a way to communicate with citra client and host a mmorpg for up to 500 connections in a server
+print("Editing the dream team cia to start the game")
+dreamteamcia=("0x80E30000")
+dreamteamlocation=("http://127.0.0.1:8080/")
+print("Starting the game...")
+print("Connecting to the server...")
+serverlocation=("https//citra.net/citra/multiplayer/")
+print("Loading dream team and citra servers...")
+print(dreamteamlocation,serverlocation)
+print("Starting citra client...")
+# ask the user if the rom is in the citra folder
+# if the rom is in the citra folder then start the game
+print("Wahoo, is the rom in the folder?")
+print("Yes or No")
+citra=input("")
+# if the rom is not in the citra folder then ask the user if they want to download the rom
+print("What is the name of the citra folder?")
+
+## decrypt the mario and luigi dream team rom files into citra client files
+import base64
+# use base64 to decrypt cia and make it playable
+base64.decrypt(input="Enter the cia file name")
+## star the citra client and lauch dream team
+_os.system("start citra")
